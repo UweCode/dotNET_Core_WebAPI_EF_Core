710283f81ffef60ca6fc47e9a19f30063cb52443
UweCode <UweCode>
2020-12-21 19:10:05 -0800

    Make controller and service async.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,18 +1,46 @@
-
+using System.Collections.Generic;
+using System.Linq;
 using Udemy_NetCore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Udemy_NetCore.Services.CharacterService;
+using System.Threading.Tasks;
 
 namespace Udemy_NetCore.Controllers
 {
     [ApiController]
-    [Route("[Controller]")]
-    public class CharacterController: ControllerBase
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
     {
-        private static Character knight = new Character();
+        private readonly ICharacterService _characterService;
 
-        public IActionResult Get()
+        public CharacterController(ICharacterService characterService)
         {
-            return Ok(knight);
+            _characterService = characterService;
+
         }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _characterService.GetAllCharacters());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingle(int id)
+        {
+            return Ok(await _characterService.GetCharacterById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCharacter(Character newCharacter)
+        {
+            return Ok(await _characterService.AddCharacter(newCharacter));
+        }
+
+        //        [HttpDelete]
+        //        public IActionResult RemoveCharacter(Character character)
+        //        {
+        //            return characters.Remove(character) ?  Ok() : BadRequest();
+        //        }
     }
 }
